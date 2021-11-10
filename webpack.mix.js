a6003259ea26831c3e7f06313983ea681b3dce1e
reza harefa <muttaqinyoga@gmail.com>
2021-11-10 17:13:31 +0700

    remove quiz.json

--- webpack.mix.js
+++ webpack.mix.js
@@ -8,7 +8,7 @@ let mix = require('laravel-mix');
  | Mix provides a clean, fluent API for defining some Webpack build steps
  | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
- |
+ |   
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
